fafc9347c78b4b3e5dfa681c388bc0256c21ebdf
Leon Si <leon@leonsilicon.com>
2026-03-27 11:07:34 -0400

    init

---
+++ src/chrome.ts
@@ -0,0 +1,47 @@
+import spawn from "nano-spawn-compat";
+import { join } from "desm";
+import { onAppLaunch, onAppTerminate, setAppNeedsRelaunch } from "chord";
+import getPort from "get-port";
+
+export default async function createChromiumHandler(meta: ImportMeta, appPath: string) {
+  let isPendingRestart = false;
+  let remoteDebuggingPort: number | null = null;
+
+  // TODO: check
+  setAppNeedsRelaunch(meta.bundleId, true);
+
+  const hasRemoteDebuggingPort = async (pid: number) => {
+    const { stdout } = await spawn("ps", ["-p", pid.toString(), "-o", "command="]);
+    return stdout.includes("remote-debugging-port");
+  };
+
+  // Ensures the app is launched with remote debugging
+  onAppLaunch(meta.bundleId, async (app) => {
+    if (!(await hasRemoteDebuggingPort(app.pid))) {
+      isPendingRestart = true;
+      await spawn("kill", ["-15", app.pid.toString()]);
+    }
+  });
+
+  onAppTerminate(meta.bundleId, async () => {
+    if (isPendingRestart) {
+      isPendingRestart = false;
+      remoteDebuggingPort = await getPort();
+      await spawn("open", [
+        "-na",
+        appPath,
+        "--args",
+        `--remote-debugging-port=${remoteDebuggingPort}`,
+      ]);
+    }
+  });
+
+  const chromeRemoteInterfaceBinPath = join(import.meta.url, "bin/chrome-remote-interface");
+  return async function (code: string) {
+    if (!remoteDebuggingPort) {
+      return false;
+    }
+
+    await spawn(chromeRemoteInterfaceBinPath, [remoteDebuggingPort.toString()], { stdin: { string: code } });
+  };
+}
